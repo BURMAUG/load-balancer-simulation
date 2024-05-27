f0033e4ecb9f903240db6e096e8841b87b88cbbc
Burmau Garba <burmaud1@gmail.com>
2024-05-26 21:46:26 -0400

    little bad

--- test/lb/LoadBalancersTest.java
+++ test/lb/LoadBalancersTest.java
@@ -1,4 +1,55 @@
-import static org.junit.jupiter.api.Assertions.*;
+package lb;
+
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Properties;
+
 class LoadBalancersTest {
-  
+    private LoadBalancers loadBalancers;
+
+    @BeforeEach
+    void setUp() {
+        Property property = new Property(new Properties());
+        loadBalancers = new LoadBalancers(property);
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void roundRobinRouting() {
+    }
+
+    @Test
+    void stickyRoundRobingRouting() {
+    }
+
+    @Test
+    void getServerErrLogFilePathDoesNotExist() {
+        String file  = loadBalancers.getServerErrLogFilePath();
+        Assertions.assertEquals(file, "Does Not Exist.");
+    }
+
+    @Test
+    void getServerErrLogFilePathExist() {
+        String file  = loadBalancers.getServerErrLogFilePath();
+        Assertions.assertEquals(file,  loadBalancers.getServerErrLogFilePath());
+    }
+
+    @Test
+    void getServerLogFilePathDoesNotExist() {
+        String filename = loadBalancers.getServerLogFilePath();
+        Assertions.assertEquals(filename, loadBalancers.getServerLogFilePath());
+    }
+
+    @Test
+    void getServerLogFilePathExist() {
+        String filename = loadBalancers.getServerLogFilePath();
+        Assertions.assertEquals(filename, loadBalancers.getServerLogFilePath());
+    }
 }
